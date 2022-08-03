161c48f5617b52598a1f0225f8259df650184f08
hudaverdik <hudaverdik@itu.edu.tr>
2022-08-03 23:28:37 +0300

    git push

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,8 +1,10 @@
 import { useHistory } from 'react-router-dom';
 import QuoteForm from '../components/quotes/QuoteForm';
+import useHTTP from '../hooks/use-http'
 
 
 const NewQuote = () => {
+    useHTTP()
     const history = useHistory();
     const addquoteHandler = QuoteData => {
         console.log(QuoteData);
