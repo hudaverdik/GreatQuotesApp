5399798be112a520f6c017c2d850d2b4e1dd4ebb
hudaverdik <hudaverdik@itu.edu.tr>
2022-08-03 23:22:58 +0300

    git push

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,8 +1,8 @@
-import { Fragment } from 'react';
-import { useHistory, useLocation } from 'react-router-dom';
+import { Fragment } from "react";
+import { useHistory, useLocation } from "react-router-dom";
 
-import QuoteItem from './QuoteItem';
-import classes from './QuoteList.module.css';
+import QuoteItem from "./QuoteItem";
+import classes from "./QuoteList.module.css";
 
 const sortQuotes = (quotes, ascending) => {
   return quotes.sort((quoteA, quoteB) => {
@@ -20,17 +20,23 @@ const QuoteList = (props) => {
 
   const queryParams = new URLSearchParams(location.search);
 
-  const isSortingAscending = queryParams.get('sort') === 'asc';
+  const isSortingAscending = queryParams.get("sort") === "asc";
 
-  const sortedQuotes = sortQuotes(props.quotes, isSortingAscending)
+  const sortedQuotes = sortQuotes(props.quotes, isSortingAscending);
 
   const changeSortingHandler = () => {
-    history.push('/quotes?sort=' + (isSortingAscending ? 'desc': 'asc'));
-  }
+    history.push({
+      pathname: location.pathname,
+      search: `?sort=${isSortingAscending ? "desc" : "asc"
+      }`,
+    });
+  };
   return (
     <Fragment>
       <div className={classes.sorting}>
-        <button onClick={changeSortingHandler}>Sort {isSortingAscending? 'descending':'ascending'}</button>
+        <button onClick={changeSortingHandler}>
+          Sort {isSortingAscending ? "descending" : "ascending"}
+        </button>
       </div>
       <ul className={classes.list}>
         {sortedQuotes.map((quote) => (
--- src/pages/QuoteDetails.js
+++ src/pages/QuoteDetails.js
@@ -1,7 +1,7 @@
 import { Fragment } from "react";
-import { Route, useParams } from "react-router-dom";
-import Comments from '../components/comments/Comments'
-import HighlightedQuote from '../components/quotes/HighlightedQuote'
+import { Link, Route, useParams, useRouteMatch } from "react-router-dom";
+import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
 const DUMMY_QUOTES = [
   { id: "q1", author: "Hako", text: "React çalış React!" },
@@ -10,18 +10,27 @@ const DUMMY_QUOTES = [
 ];
 
 const QuoteDetails = () => {
+  const match = useRouteMatch();
   const params = useParams();
-  const quote = DUMMY_QUOTES.find(quote => quote.id===params.quoteId)
-  
-  if(!quote) {
-    return <p>No quote found!</p>
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote found!</p>;
   }
-  
+
   return (
     <Fragment>
-      <HighlightedQuote text={quote.text} author={quote.author}/>
-      <Route path={`/quotes/${params.quoteId}/comments`}>
-        <Comments/>
+      <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={`${match.path}`} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`${match.url}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
+
+      <Route path={`${match.path}/comments`}>
+        <Comments />
       </Route>
     </Fragment>
   );
