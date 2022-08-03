1d4e360ec9994c8ad753d6e68c576e3e9e3504ef
hudaverdik <hudaverdik@itu.edu.tr>
2022-08-03 21:36:52 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { Redirect, Route, Switch } from "react-router-dom";
+import AllQuotes from "./pages/AllQuotes";
+import QuoteDetails from "./pages/QuoteDetails";
+import NewQuote from "./pages/NewQuote";
+import Layout from "./components/layout/Layout";
+
+function App() {
+  return (
+    <Layout>
+    <Switch>
+      <Route path='/' exact>
+        <Redirect to='/quotes'></Redirect>
+      </Route>
+      <Route path="/quotes" exact>
+        <AllQuotes/>
+      </Route>
+      <Route path="/quotes/:quoteId">
+        <QuoteDetails/>
+      </Route>
+      <Route path="/new-quote">
+        <NewQuote/>
+      </Route>
+    </Switch>
+    </Layout>
+  );
+}
+
+export default App;
---
+++ src/pages/QuoteDetails.js
@@ -0,0 +1,18 @@
+import { Fragment } from "react";
+import { Route, useParams } from "react-router-dom";
+import Comments from '../components/comments/Comments'
+
+const QuoteDetails = () => {
+  const params = useParams();
+  return (
+    <Fragment>
+      <h1>Quote Details Page.</h1>
+      <p>{params.quoteId}</p>
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments/>
+      </Route>
+    </Fragment>
+  );
+};
+
+export default QuoteDetails;
